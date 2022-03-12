65f560818a14c6588dc200a466d1b9a24256ebc8
Ritvik <77543865+greyhatguy007@users.noreply.github.com>
2022-03-12 15:36:08 +0530

    Update 1.c

--- Week8/Tutorials/1.c
+++ Week8/Tutorials/1.c
@@ -1,1 +1,31 @@
- 
+// Write a program to compare two dates entered by user. Make a structure named Date to store the elements day, month and year to store the dates. If the dates are equal, display "Dates are equal" otherwise display "Dates are not equal".
+
+// Input Format:
+
+// First line contains first date in dd mm yyyy format
+
+// Second line contains first date in dd mm yyyy format
+
+// Output Format:
+
+// display the result as Dates are equal  or Dates are not equal
+
+
+
+#include <stdio.h>
+struct date{
+    int date;
+    int month;
+    int year;
+}s[2];
+int main(){
+    for(int i=0;i<2;i++){
+        scanf("%d %d %d",&s[i].date,&s[i].month,&s[i].year);
+    }
+    if(s[0].date==s[1].date){
+        printf("Dates are equal");
+    }
+    else{
+        printf("Dates are not equal");
+    }
+    }
