f4dc7118675abe78f8d320c8ef352062f4de2658
Ritvik <77543865+greyhatguy007@users.noreply.github.com>
2022-03-12 15:40:50 +0530

    Create 6.c

---
+++ Week7/Lab_Exercise/6.c
@@ -0,0 +1,142 @@
+// Given an array of integers, reverse the given array in place using an index and loop rather than a built-in function.
+
+ 
+
+// Example
+
+ 
+
+// arr = [1, 3, 2, 4, 5]
+
+ 
+
+// Return the array [5, 4, 2, 3, 1] which is the reverse of the input array.
+
+ 
+
+// Function Description
+
+ 
+
+// Complete the function reverseArray in the editor below.
+
+ 
+
+// reverseArray has the following parameter(s):
+
+// int arr[n]:  an array of integers
+
+// Return
+
+// int[n]: the array in reverse order
+
+ 
+
+// Constraints
+
+ 
+
+// 1 ≤ n ≤ 100
+
+// 0 < arr[i] ≤ 100
+
+ 
+
+// Input Format For Custom Testing
+
+ 
+
+// The first line contains an integer, n, the number of elements in arr.
+
+ 
+
+// Each line i of the n subsequent lines (where 0 ≤ i < n) contains an integer, arr[i].
+
+ 
+
+// Sample Case 0
+
+ 
+
+// Sample Input For Custom Testing
+
+ 
+
+// 5
+
+// 1
+
+// 3
+
+// 2
+
+// 4
+
+// 5
+
+ 
+
+// Sample Output
+
+ 
+
+// 5
+
+// 4
+
+// 2
+
+// 3
+
+// 1
+
+ 
+
+
+ 
+
+// Explanation
+
+ 
+
+// The input array is [1, 3, 2, 4, 5], so the reverse of the input array is [5, 4, 2, 3, 1].
+
+ 
+
+// Sample Case 1
+
+// Sample Input For Custom Testing
+
+ 
+
+// 4
+
+// 17
+
+// 10
+
+// 21
+
+// 45
+
+ 
+
+// Sample Output
+
+ 
+
+// 45
+
+// 21
+
+// 10
+
+// 17
+
+ 
+
+// Explanation
+
+ 
+
+// The input array is [17, 10, 21, 45], so the reverse of the input array is [45, 21, 10, 17].
