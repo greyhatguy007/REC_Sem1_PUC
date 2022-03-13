32fb40f6585b4b0330bf207059d976300328ffef
Ritvik <77543865+greyhatguy007@users.noreply.github.com>
2022-03-13 20:34:23 +0530

    Update 3.c

--- Week7/Lab_exercise_2/3.c
+++ Week7/Lab_exercise_2/3.c
@@ -1,1 +1,44 @@
  
+/*
+ * Complete the 'reverseArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+/* THIS IS NOT WORKING */
+
+
+/*
+ * To return the integer array from the function, you should:
+ *     - Store the size of the array to be returned in the result_count variable
+ *     - Allocate the array statically or dynamically
+ *
+ * For example,
+ * int* return_integer_array_using_static_allocation(int* result_count) {
+ *     *result_count = 5;
+ *
+ *     static int a[5] = {1, 2, 3, 4, 5};
+ *
+ *     return a;
+ * }
+ *
+ * int* return_integer_array_using_dynamic_allocation(int* result_count) {
+ *     *result_count = 5;
+ *
+ *     int *a = malloc(5 * sizeof(int));
+ *
+ *     for (int i = 0; i < 5; i++) {
+ *         *(a + i) = i + 1;
+ *     }
+ *
+ *     return a;
+ * }
+ *
+ */
+int* reverseArray(int arr_count, int *arr, int *result_count) {
+int *a = malloc(5 * sizeof(int));
+ 
+     for (int i = 0; i < 5; i++) {
+        *(a + i) = i + 1;     } 
+        return a;
+}
