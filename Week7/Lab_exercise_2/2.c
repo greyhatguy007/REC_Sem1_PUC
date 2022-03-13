8a898b7512edb9761126d7f3bc4e16a91834c3fb
Ritvik <77543865+greyhatguy007@users.noreply.github.com>
2022-03-13 20:33:50 +0530

    Update 2.c

--- Week7/Lab_exercise_2/2.c
+++ Week7/Lab_exercise_2/2.c
@@ -1,1 +1,24 @@
  
+/*
+ * Complete the 'balancedSum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+#include<string.h>
+int balancedSum(int arr_count, int* arr) 
+{
+    int sum=0;
+    for(int i=0;i<arr_count;i++)
+    sum=sum+arr[i];
+    int sum1=0;
+    sum=sum-arr[arr_count-1];
+    for(int i=arr_count-2;i>0;i--)
+    {
+        sum1=sum1+arr[i+1];
+        sum=sum-arr[i];
+        if(sum==sum1)
+        return i;
+    }
+    return -1;
+}
